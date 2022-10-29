3dc7ba5a9580405ef081566ca838815ec84187d4
Alexander Spichak <aspichak@eagles.ewu.edu>
2022-10-29 14:45:42 -0700

    Add Bootstrap 5

--- app/frontend/Pages/Storefront/Index.vue
+++ app/frontend/Pages/Storefront/Index.vue
@@ -13,9 +13,28 @@
 </script>
 
 <template>
-  <div v-for="b in inventory">
-    <h2>{{ b.name }}</h2>
-    <p>{{ b.description }}</p>
-    <p>${{ b.price }}</p>
+  <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+    
+    <!-- Product card-->
+    <div class="col mb-5" v-for="b in inventory">
+        <div class="card h-100">
+            <!-- Product image-->
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder">{{ b.name }}</h5>
+                    <!-- Product price-->
+                    ${{ b.price }}
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+            </div>
+        </div>
+    </div>
+  
   </div>
 </template>
