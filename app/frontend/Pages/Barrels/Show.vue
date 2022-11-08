948d2b7dbe1acf75f5aa241cf3cfa18c55d07d25
Alexander Spichak <aspichak@eagles.ewu.edu>
2022-11-07 18:48:03 -0800

    Add volume to product details page

--- app/frontend/Pages/Barrels/Show.vue
+++ app/frontend/Pages/Barrels/Show.vue
@@ -15,19 +15,19 @@
 </script>
 
 <template>
-  <!-- TODO: Add barrel volume -->
   <section class="py-5">
       <div class="container px-4 px-lg-5 my-5">
           <div class="row gx-4 gx-lg-5">
               <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" :src="barrel.image_url" alt="Barrel image" /></div>
               <div class="col-md-6">
                   <div class="small mb-1">SKU: BRL-{{ ('0000' + barrel.id).slice(-4) }}</div>
-                  <h1 class="display-5 fw-bolder">{{ barrel.name }}</h1>
-                  <div class="fs-5 mb-5">
+                  <h1 class="display-5 fw-bolder mb-0">{{ barrel.name }}</h1>
+                  <div class="fs-5 mb-4">
                       <span><Currency :value="barrel.price"/></span>
                   </div>
                   <p v-if="barrel.description" class="lead" v-html="barrel.description"></p>
                   <p v-else class="lead">No description provided</p>
+                  <p>Volume: {{ barrel.volume }} Gal</p>
                   <div class="d-flex">
                       <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
                       <button class="btn btn-outline-dark flex-shrink-0" type="button">
