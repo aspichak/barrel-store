1d981a6fd05471eee6524cb4cb432d47772b65c9
Alexander Spichak <aspichak@eagles.ewu.edu>
2022-11-09 22:05:42 -0800

    Fix formatting in StorefrontController

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -2,8 +2,18 @@ class StorefrontController < ApplicationController
   def index
     render inertia: 'Storefront/Index', props: {
       inventory: Barrel.with_attached_image.all.map do |barrel|
-        barrel.as_json.merge({ image_url: barrel.image.attached? ? url_for(barrel.image.variant(resize_to_fill: [260, 260])) : 'https://via.placeholder.com/160/fff/fff' })
+        barrel.as_json.merge({
+          image_url: barrel.image.attached? ?
+            url_for(barrel.image.variant(resize_to_fill: [260, 260])) :
+            'https://via.placeholder.com/160/fff/fff'
+        })
       end
+    }
+  end
+
+  def cart
+    render inertia: 'Storefront/Index', props: {
+      cart: Barrel.all
     }
   end
 end
